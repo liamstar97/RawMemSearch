422bd680469a62b567a787b44b85c3509df8b940
Liam Thompson <liamstar@gmaill.com>
2018-08-23 14:21:59 -0700

    added maven build tool

---
+++ rawmemsearch/src/main/java/org/rawmemsearch/QueryDocument.java
@@ -0,0 +1,8 @@
+package org.rawmemsearch;
+
+import java.util.*;
+
+public class QueryDocument {
+
+  //  public QueryDocument(List<TermDocument> termDocuments, String query, )
+}
---
+++ rawmemsearch/src/test/java/org/rawmemsearch/AppTest.java
@@ -0,0 +1,24 @@
+package org.rawmemsearch;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    public void searchReturnsNonEmptyResults() {
+
+    }
+}

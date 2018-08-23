ef9a9aeaf3562440ee15b322df6bdd6402131fac
Jake Mannix <jake.mannix@gmail.com>
2018-08-23 13:16:03 -0700

    parametrized max number of files

--- src/org/rawmemsearch/Main.java
+++ src/org/rawmemsearch/Main.java
@@ -8,21 +8,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String path = args.length > 0 ? args[0] : "/home/corrupt/Desktop/books";
         String query = args.length > 1 ? args[1] : "whenever";
-        SlightlySmarterSearch slightlySmarterSearch = new SlightlySmarterSearch(files(path));
+        SlightlySmarterSearch slightlySmarterSearch = new SlightlySmarterSearch(files(path, 5));
         for (SearchDocument searchDocument: slightlySmarterSearch.search(query, 10)) {
             System.out.println(searchDocument.getTitle() + " " + searchDocument.getScore());
         }
 
     }
 
-    private static List<File> files(String path) {
+    private static List<File> files(String path, int max) {
         File directory = new File(path);
         if (directory.isDirectory()) {
             File[] files = directory.listFiles();
             if (files == null) {
                 throw new IllegalArgumentException("there were no files in the directory " + directory.toString());
             }
-            return Arrays.asList(files).subList(0, 5);
+            List<File> fileList = Arrays.asList(files);
+            return max < 0 ? fileList : fileList.subList(0, max);
         } else {
             throw new IllegalArgumentException("not a directory!");
         }
